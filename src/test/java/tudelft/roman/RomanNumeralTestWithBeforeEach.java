53577470b14de165a2d471af4324187587ab88e2
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2025-01-16 16:22:25 +0100

    Add more Test using the method annotation @BeforeEach.

--- src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
+++ src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
@@ -6,12 +6,26 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTestWithBeforeEach {
 
+    // We can avoid repetition, so we can use @BeforeEach annotation.
+    // Methods with that annotation are executed before each individual test.
     private RomanNumeral roman;
-
     @BeforeEach
     public void initialize() {
         this.roman = new RomanNumeral();
     }
+
+    @Test
+    public void singleNum(){
+        int result = roman.convert("V");
+        Assertions.assertEquals(5,result);
+    }
+
+    @Test
+    public void numWithManyDig(){
+        int result = roman.convert("XIII");
+        Assertions.assertEquals(13, result);
+    }
+
 
     @Test
     public void singleNumber() {
@@ -24,6 +38,13 @@ public class RomanNumeralTestWithBeforeEach {
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
+
+    @Test
+    public void numWithSubNotation(){
+        int result = roman.convert("IX");
+        Assertions.assertEquals(9, result);
+    }
+
 
     @Test
     public void numberWithSubtractiveNotation() {
