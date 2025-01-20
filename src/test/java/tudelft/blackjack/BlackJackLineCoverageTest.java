12990026d385337ea504e8dac89e9d1c72d40a14
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2025-01-20 13:00:30 +0100

    Structural testiong and code coverage.

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -16,4 +16,10 @@ public class BlackJackLineCoverageTest {
         int result = new BlackJack().play(10, 9);
         Assertions.assertEquals(10, result);
     }
+
+    @Test
+    public void rightPlayerWins(){
+        int result = new BlackJack().play(8,20);
+        Assertions.assertEquals(20,result);
+    }
 }
